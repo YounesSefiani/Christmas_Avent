78d0e4dca0a135842296b8bd10a5e579546c8630
Echeverria-Didier <didbidean@hotmail.com>
2023-11-24 11:17:45 +0100

    transparence de la page

--- src/components/CalendarCase.jsx
+++ src/components/CalendarCase.jsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import React from "react";
 
-const CalendarCase = ({title, poster, background, director, casting, synopsis, genre, streaming, duration, release_date, trailer }) => {
+const CalendarCase = ({
+  title,
+  poster,
+  background,
+  director,
+  casting,
+  synopsis,
+  genre,
+  streaming,
+  duration,
+  release_date,
+  trailer,
+}) => {
   return (
     <div className="case-calendrier">
       <div className="background">
-        <img src={background} alt={`Background de ${title}`}/>
+        <img src={background} alt={`Background de ${title}`} />
       </div>
       <div className="poster">
         <img src={poster} alt={`Affiche de ${title}`} />
       </div>
       <h2>{title}</h2>
-      <p>{genre} / {release_date} / {duration}</p>
+      <p>
+        {genre} / {release_date} / {duration}
+      </p>
       <p>Réalisé par {director}</p>
       <p>Avec {casting}</p>
       <p>{synopsis}</p>
       <p> Disponible sur {streaming}.</p>
-      <iframe width="460" height="315" src={trailer} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe
+        width="460"
+        height="315"
+        src={trailer}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
     </div>
   );
 };
 
 export default CalendarCase;
-
